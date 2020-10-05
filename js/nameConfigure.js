254ab5ca44851714d9bf2b9affff12f4d43ef39d
oroafrica <moegamat@oroafrica.com>
2020-10-05 18:46:43 +0200

    module _res17

---
+++ js/nameConfigure.js
@@ -0,0 +1,30 @@
+export config={
+global:{screen:"rgba(231,245,249,0.8)",chainWidth:240,cw:420,ch:250}
+,bname:{fontName:"cname",fontSize:35,gemSize:14,gemRadius:14/2,pua:60000}
+,cname:{fontName:"cname",fontSize:35,gemSize:14,gemRadius:14/2,pua:60000}
+,mname:{fontName:"cname",fontSize:35,gemSize:14,gemRadius:14/2,pua:60000}
+}
+
+export function inputs()
+{
+	let user = {};
+	user.txt1 = $("input[title='Enter your name here']");
+		
+	let tgt = $('select').toArray();
+	$.each(tgt, (index)=> 
+	{
+		if($(tgt).eq(index).find("option").length > 2 && $(tgt)[index].options[1].text === "Silver")
+		{
+			user.alloy = $(tgt)[index];
+		}
+		if($(tgt).eq(index).find("option").length > 2 && $(tgt)[index].options[1].text === "Heart")
+		{
+			user.motif = $(tgt)[index];
+		}
+		if($(tgt).eq(index).find("option").length > 2 && $(tgt)[index].options[1].text === "Genuine Diamond")
+		{
+			user.accent = $(tgt)[index];
+		}
+	}); 
+	return user;
+}
--- js/scriptName.js
+++ js/scriptName.js
@@ -1,14 +1,51 @@
 import {res} from "./resources.js";
+import {config, inputs} from "./nameConfigure.js";
 
 export class Store
 {
 	constructor()
 	{
+		//boolean
+		this.dev=true;
+		this.canvas = new fabric.Canvas('canvas',{backgroundColor:"rgba(231,245,249,0.8)",width:config.global.cw,height:config.global.ch,objectCaching:false});
+		
+		this.pendant;
+	
+	}
+	
+	msg(a)
+	{
+		console.log(a);
+	}
+	
+	debug()
+	{
+		this.msg("Hello name store");
+	}
+	setBounds(ok)
+	{	
+
+	}
+	
+	draw()
+	{
 		
 	}
 	
-	msg()
+	render()
 	{
-		console.log("hi from store");
+		render()
+		{	
+			$(document).on("keyup",$(this.uTxt),(e)=>
+			{
+				this.draw();
+				this.debug();
+			})
+			.on("change",$(this.uAlloy),(e)=>
+			{
+				this.draw();
+				this.debug();
+			});
+		}
 	}
 }
