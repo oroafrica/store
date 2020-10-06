6f3f805e4b356c245cc9c908cf78a2b0a5020a3e
moegamat <moegamat@oroafrica.com>
2020-10-06 20:52:58 +0200

    set x

--- js/scriptName.js
+++ js/scriptName.js
@@ -89,7 +89,15 @@ export class Store
 		if(this.renderBounds) this.canvas.add(aBox).add(bBox).add(cBox);
 	}
 
-
+	gemMap()
+	{
+		let radius = this.con.gemRadius;
+		let z = this.item.bounds.width+this.item.bounds.left;
+		let gx = {0:0, 1:0, 2:0, 3:0, 4:122+radius,5:119+radius,6:117+radius,7:115+radius};
+		let gy = {0:0, 1:0, 2:0, 3:0, 4:(z-15+radius),5:(z-22+radius),6:(z-17+radius),7:(z-25+radius)};
+		let tg = $(this.user.uMotif).prop("selectedIndex");
+		return ($(this.user.accent).prop("selectedIndex") === 0) ? {y:Object.values(gx)[tg],x:Object.values(gy)[tg]} : {y:Object.values(gx)[tg+4],x:Object.values(gy)[tg+4]};
+	}
 	draw()
 	{
 		this.canvas.clear();
@@ -128,7 +136,7 @@ export class Store
 		fabric.Image.fromURL(res.accent($(this.user.accent).prop("selectedIndex")),(im)=>
 		{
 			im.scaleToWidth(this.con.gemSize);
-			im.set({left:100,top:150,originX:"center",originY:"center",objectCaching:false,noScaleCache:false,selectable:false
+			im.set({left:this.gemMap().x,top:150,originX:"center",originY:"center",objectCaching:false,noScaleCache:false,selectable:false
 			,shadow:"rgba(0,0,0,1) 0 0 2"});
 			if(this.allfix !== "") 
 			{
