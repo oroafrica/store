ec418b3864e1a46d2d211409d95b78eb7346dc18
moegamat <moegamat@oroafrica.com>
2020-10-07 04:29:07 +0200

    abstract bounds

--- js/scriptName.js
+++ js/scriptName.js
@@ -91,10 +91,10 @@ export class Store
 
 	gemMap()
 	{
-		let radius = this.con.gemRadius;
+		let radius = this.con.gemRadius; 
 		let z = this.item.bounds.width+this.item.bounds.left;
-		let gx = {0:0, 1:0, 2:0, 3:0, 4:122+radius,5:119+radius,6:117+radius,7:115+radius};
-		let gy = {0:0, 1:0, 2:0, 3:0, 4:(z-15+radius),5:(z-22+radius),6:(z-17+radius),7:(z-25+radius)};
+		let gx = this.con.accentMap(this.item.bounds).y; //{0:0, 1:0, 2:0, 3:0, 4:122+radius,5:119+radius,6:117+radius,7:115+radius};
+		let gy = this.con.accentMap(this.item.bounds).x; //{0:0, 1:0, 2:0, 3:0, 4:(z-15+radius),5:(z-22+radius),6:(z-17+radius),7:(z-25+radius)};
 		let tg = $(this.user.motif).prop("selectedIndex");
 		return ($(this.user.accent).prop("selectedIndex") === 0) ? {y:Object.values(gx)[tg],x:Object.values(gy)[tg]} : {y:Object.values(gx)[tg+4],x:Object.values(gy)[tg+4]};
 	}
@@ -136,7 +136,7 @@ export class Store
 		fabric.Image.fromURL(res.accent($(this.user.accent).prop("selectedIndex")),(im)=>
 		{this.msg(this.con.accentMap(this.item.bounds).x);
 			im.scaleToWidth(this.con.gemSize);
-			im.set({left:this.con.accentMap(this.item.bounds).x,top:this.gemMap().y,originX:"center",originY:"center",objectCaching:false,noScaleCache:false,selectable:false
+			im.set({left:this.gemMap().x,top:this.gemMap().y,originX:"center",originY:"center",objectCaching:false,noScaleCache:false,selectable:false
 			,shadow:"rgba(0,0,0,1) 0 0 2"});
 			if(this.allfix !== "") 
 			{
